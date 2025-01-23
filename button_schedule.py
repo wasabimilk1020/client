ae179c5dd6fd70c53fdd5e1487d945ff5bb91c6d
Eunsu Kim <wntuwntu@gmail.com>
2025-01-23 11:49:23 +0900

    특별한 사항없이 저장

--- button_schedule.py
+++ button_schedule.py
@@ -1,13 +1,12 @@
 import time
-ERR_MSG = 0
-STATUS_MSG = 1
+
 
 def postBox(sio, data, id):
   character_name=id
   print("우편함")
-  print("data: ", data)
-  sio.emit("logEvent",["우편실행", character_name, ERR_MSG])
   time.sleep(1)
+  return 0
 
 def dungeon(sio, data):
   print("격전의 섬")
+  return 1
--- mainloop.py
+++ mainloop.py
@@ -1,5 +1,13 @@
+ERR_MSG = 0
+STATUS_MSG = 1
+
 def mainLoop(sio, btn_func, func_data, id_handle, btn_name):
   for id, handle in id_handle.items():
     character_name=id
-    btn_func(sio, func_data, character_name)
+    result=btn_func(sio, func_data, character_name)
+    if result==1:
+      sio.emit("logEvent",[f"{btn_name} 실행", character_name, STATUS_MSG])
+    elif result==0:
+      sio.emit("logEvent",[f"{btn_name} 실패", character_name, ERR_MSG])
+
     
